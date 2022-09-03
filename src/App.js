8b17d1a5e28d40d075f479b3b5707f2862c62ac4
Macheteros <layefa4life@gmail.com>
2022-09-03 20:07:21 +0100

    first wave

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function App() {
+  return (
+    <div>
+      <form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+      </form>
+    </div>
+  );
+}
