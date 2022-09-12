95aedba7b32b89c4795770a4f11d14cfcdb81a34
Macheteros <layefa4life@gmail.com>
2022-09-12 13:26:41 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from "react";
-import Profile from "./Component/profile";
-import PropTypes from "prop-types";
+import React from 'react';
+import Navbar from './components/navbar';
 
 export default function App() {
-  const handleName = (fullName) => alert(fullName);
-  return (
-    <div>
-      <Profile
-        handleName={handleName}
-        fullName="Martins Orubu"
-        bio="I am a tall black Nigerian."
-        profession="I am a fullstack developer." >
 
-        <img src="/Image/photo_2022-05-18_12-34-48.jpg" alt="me"/>
-      </Profile>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fullName: "Martins Orubu",
+    bio: "Drone pilot and fullstack developer.",
+    imgSrc: "",
+    profession: "Safety officer.",
+    show: true,
+    time: 0,
+  };
+
+
+toggleShow = () => {
+  this.setState((prevProfile) => ({ show: !prevProfile.show }));
+  this.state.show
+    ? this.componentDidMount()
+    : this.setState({ time: 0 }) && this.componentWillUnmount();
+};
+
+componentDidMount() {
+  this.myInterval = setInterval(() => {
+    this.setState((prevState) => ({ time: prevState.time + 1 }));
+  }, 1000);
+}
+componentWillUnmount() {
+  clearInterval(this.myInterval);
 }
 
-Profile.PropTypes = {
-  handleName: PropTypes.func,
-  fullName: PropTypes.string,
-  bio: PropTypes.string,
-  profession: PropTypes.string,
-}
+render() {
+  <div className="screen">
+      <Navbar />
+      {this.state.show == true && (
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={require("./components/photo_2022-05-18_12-34-48.jpg")}
+                className="img-fluid rounded-start"
+                alt="Me"
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{this.state.fullName}</h5>
+                <h6 className="card-title">{this.state.profession}</h6>
+                <p className="card-text">{this.state.bio}</p>
+                  <small className="text-muted">Last mounted: {this.state.time} seconds ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      
+      <div className="btn-div">
+        <button type="button" class="btn btn-dark" onClick={this.toggleShow}>
+          {this.state.show == true ? "Hide" : "Show"}
+        </button>
+      </div>
+      </div>
+      } 
+    } }
